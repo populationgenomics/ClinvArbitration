b90b732eb6acdafa65d15046e12c631e2d7eb1da
MattWellie <mattwellie@gmail.com>
2024-03-06 14:27:16 +1000

    find additional hidden variants, add tests

---
+++ scripts/test_resummarise.py
@@ -0,0 +1,30 @@
+import zoneinfo
+from copy import deepcopy
+from datetime import datetime
+
+import pytest
+from resummarise import Consequence, Submission, check_stars, consequence_decision
+
+TIMEZONE = zoneinfo.ZoneInfo('Australia/Brisbane')
+BASIC_SUB = Submission(datetime.now(tz=TIMEZONE), 'foo', Consequence.UNKNOWN, 'review')
+
+
+@pytest.mark.parametrize(
+    'consequences,expected',
+    [
+        ([Consequence.PATHOGENIC], Consequence.PATHOGENIC),
+        ([Consequence.PATHOGENIC, Consequence.PATHOGENIC], Consequence.PATHOGENIC),
+        ([Consequence.BENIGN], Consequence.BENIGN),
+        ([Consequence.BENIGN, Consequence.PATHOGENIC], Consequence.CONFLICTING),
+    ],
+)
+def test_consequence_decision_path_single(
+    consequences: list[Consequence],
+    expected: Consequence,
+):
+    all_subs = []
+    for con in consequences:
+        sub = deepcopy(BASIC_SUB)
+        sub.classification = con
+        all_subs.append(sub)
+    assert consequence_decision(all_subs) == expected
