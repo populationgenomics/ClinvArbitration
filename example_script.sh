dea82d1156cef1717bee605902b017af02227005
MattWellie <mattwellie@gmail.com>
2024-04-26 11:50:42 +1000

    update to example execution

--- example_script.sh
+++ example_script.sh
@@ -15,7 +15,7 @@ wget -O data/submission_summary.txt.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/
 
 # run the docker image to generate the summarised output
 docker run --platform linux/amd64 -v "$(pwd)/data":/data hail_clinvar:example \
-    /bin/bash -c "python3 /scripts/resummarise.py -v /data/variant_summary.txt.gz -s /data/submission_summary.txt.gz -o /data/clinvar_summary"
+    /bin/bash -c "python3 /clinvarbitration/resummarise.py -v /data/variant_summary.txt.gz -s /data/submission_summary.txt.gz -o /data/clinvar_summary"
 
 # upon completion, this will have generated files in the data directory:
 # - data/clinvar_summary.json - a JSON file containing the summarised data entries, one json object per line
@@ -23,11 +23,11 @@ docker run --platform linux/amd64 -v "$(pwd)/data":/data hail_clinvar:example \
 # - data/clinvar_summary.vcf.bgz - a bgzipped file containing the pathogenic SNV entries in VCF format
 # - data/clinvar_summary.ht - a Hail Table containing the summarised data entries
 
-# This is where you should run VEP on data/clinvar_summary.vcf.bgz, with protein consequence annotation per transcript
-# Let's imagine you did that, and the result is in data/pathogenic_annotated.vcf.bgz
-# I've enclosed a 10-variant example of this, as annotated by https://www.ensembl.org/Homo_sapiens/Tools/VEP
-docker run --platform linux/amd64 -v "$(pwd)/data":/data hail_clinvar:example \
-    /bin/bash -c "python3 /scripts/clinvar_by_codon_from_vcf.py -i /data/pathogenic_annotated.vcf.bgz -o /data/pm5"
+## This is where you should run VEP on data/clinvar_summary.vcf.bgz, with protein consequence annotation per transcript
+## Let's imagine you did that, and the result is in data/pathogenic_annotated.vcf.bgz
+## I've enclosed a 10-variant example of this, as annotated by https://www.ensembl.org/Homo_sapiens/Tools/VEP
+#docker run --platform linux/amd64 -v "$(pwd)/data":/data hail_clinvar:example \
+#    /bin/bash -c "python3 /clinvarbitration/clinvar_by_codon_from_vcf.py -i /data/pathogenic_annotated.vcf.bgz -o /data/pm5"
 
 # upon completion, this will generate files in the data directory:
 # - data/pm5.json - a JSON file containing the PM5 results, one JSON object per line
