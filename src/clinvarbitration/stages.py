e490bcf2e669009406ff88d97ad9b40a92791e10
MattWellie <mattwellie@gmail.com>
2025-04-30 09:00:49 +1000

    remove redundant nextflow stage definition

--- src/clinvarbitration/stages.py
+++ src/clinvarbitration/stages.py
@@ -70,7 +70,10 @@ class CopyLatestClinvarFiles(MultiCohortStage):
 
         outputs = self.expected_outputs(mc)
 
-        bash_job = copy_latest_files(submissions=outputs['submission_file'], variants=outputs['variant_file'])
+        bash_job = copy_latest_files(
+            submissions=outputs['submission_file'],
+            variants=outputs['variant_file'],
+        )
 
         return self.make_outputs(data=outputs, jobs=bash_job, target=mc)
 
@@ -88,8 +91,11 @@ class GenerateNewClinvarSummary(MultiCohortStage):
 
     def queue_jobs(self, mc: 'MultiCohort', inputs: 'StageInput') -> 'StageOutput':
         outputs = self.expected_outputs(mc)
+
+        # identify input files from CopyLatestClinvarFiles (raw ClinVar data)
         var_file = get_batch().read_input(inputs.as_str(mc, CopyLatestClinvarFiles, 'variant_file'))
         sub_file = get_batch().read_input(inputs.as_str(mc, CopyLatestClinvarFiles, 'submission_file'))
+
         job = generate_new_summary(
             var_file=var_file,
             sub_file=sub_file,
@@ -110,8 +116,13 @@ class AnnotateClinvarSnvsWithBcftools(MultiCohortStage):
 
     def queue_jobs(self, mc: 'MultiCohort', inputs: 'StageInput') -> 'StageOutput':
         output = self.expected_outputs(mc)
+
         snv_vcf = get_batch().read_input(inputs.as_str(mc, GenerateNewClinvarSummary, 'snv_vcf'))
-        job = annotate_clinvar_snvs(snv_vcf=snv_vcf, output=output)
+
+        job = annotate_clinvar_snvs(
+            snv_vcf=snv_vcf,
+            output=output,
+        )
         return self.make_outputs(target=mc, jobs=job, data=output)
 
 
@@ -128,6 +139,7 @@ class Pm5TableGeneration(MultiCohortStage):
 
     def queue_jobs(self, mc: 'MultiCohort', inputs: 'StageInput') -> 'StageOutput':
         outputs = self.expected_outputs(mc)
+
         annotated_snvs = inputs.as_str(mc, AnnotateClinvarSnvsWithBcftools)
 
         job = generate_pm5_data(
@@ -161,104 +173,17 @@ class PackageForRelease(MultiCohortStage):
         tarball it, and write out as a single file
         """
         output = self.expected_outputs(multicohort)
-        pm5 = inputs.as_dict(multicohort, Pm5TableGeneration)
+
+        annotated_variants_tsv = inputs.as_path(multicohort, AnnotateClinvarSnvsWithBcftools)
         clinvar_decisions = inputs.as_str(multicohort, GenerateNewClinvarSummary, 'clinvar_decisions')
+        pm5 = inputs.as_dict(multicohort, Pm5TableGeneration)
+
         job = package_data_for_release(
-            annotated_tsv=inputs.as_path(multicohort, AnnotateClinvarSnvsWithBcftools),
+            annotated_tsv=annotated_variants_tsv,
             pm5_json=pm5['pm5_json'],
             pm5_ht=pm5['pm5_ht'],
             clinvar_decisions=clinvar_decisions,
             output=output,
         )
+
         return self.make_outputs(multicohort, data=output, jobs=job)
-
-
-@stage(
-    analysis_keys=['release.tar.gz'],
-    analysis_type='clinvarbitration',
-    update_analysis_meta=populate_job_meta,
-)
-class ClinvarbitrationNextflow(MultiCohortStage):
-    """
-    Instead of us running this one way, and off-site users running it another way,
-    this single stage executes the full NextFlow workflow
-    We don't have to use this, but we should run it alongside our main workflow to ensure both are working
-    """
-
-    def expected_outputs(
-        self,
-        multicohort: 'MultiCohort',
-    ) -> 'dict[str, Path]':
-        return {
-            'submission_raw.txt.gz': get_output_folder() / 'clinvar_decisions.submission_raw.txt.gz',
-            'variant_raw.txt.gz': get_output_folder() / 'clinvar_decisions.variant_raw.txt.gz',
-            'ht.tar.gz': get_output_folder() / 'clinvar_decisions.ht.tar.gz',
-            'pm5.ht.tar.gz': get_output_folder() / 'clinvar_decisions.pm5.ht.tar.gz',
-            'vcf.bgz': get_output_folder() / 'clinvar_decisions.vcf.bgz',
-            'release.tar.gz': get_output_folder() / 'clinvar_decisions.release.tar.gz',
-        }
-
-    def queue_jobs(
-        self,
-        multicohort: 'MultiCohort',
-        inputs: 'StageInput',
-    ) -> 'StageOutput':
-        """
-
-        Args:
-            multicohort ():
-            inputs ():
-
-        Returns:
-
-        """
-
-        outputs = self.expected_outputs(multicohort)
-
-        # read in a reference genome
-        ref_fa = get_batch().read_input(config_retrieve(['workflow', 'ref_fa']))
-
-        # make a new job
-        job = get_batch().new_job('Run ClinvArbitration Nextflow')
-
-        job.image(config_retrieve(['workflow', 'driver_image']))
-
-        # set some resource params
-        job.storage('10Gi').memory('highmem').cpu(2)
-
-        # set up one sprawling output group for all workflow results
-        job.declare_resource_group(
-            output={
-                'submission_raw.txt.gz': '{root}/submission_summary.txt.gz',
-                'variant_raw.txt.gz': '{root}/variant_summary.txt.gz',
-                'clinvar_decisions.json': '{root}/clinvar_decisions.json',
-                'ht.tar.gz': '{root}/clinvar_decisions.ht.tar.gz',
-                'pm5.ht.tar.gz': '{root}/clinvar_decisions.pm5.ht.tar.gz',
-                'pm5.json': '{root}/clinvar_decisions.pm5.json',
-                'vcf.bgz': '{root}/clinvar_decisions.vcf.bgz',
-                'vcf.bgz.tbi': '{root}/clinvar_decisions.vcf.bgz.tbi',
-                'unfiltered.vcf.bgz': '{root}/clinvar_decisions.unfiltered.vcf.bgz',
-                'unfiltered.vcf.bgz.tbi': '{root}/clinvar_decisions.unfiltered.vcf.bgz.tbi',
-                'annotated.tsv': '{root}/clinvar_decisions.annotated.tsv',
-                'release.tar.gz': '{root}/clinvar_decisions.release.tar.gz',
-            },
-        )
-
-        gff3 = get_batch().read_input(config_retrieve(['references', 'ensembl_113', 'gff3']))
-
-        # nextflow go brrrr
-        job.command(
-            f"""
-            nextflow \
-                -c nextflow/nextflow.config \
-                run nextflow/clinvarbitration.nf \
-                --ref_fa {ref_fa} \
-                --output_dir {job.output} \
-                --gff3 {gff3}
-            """,
-        )
-
-        # copy the outputs back, in one smooooooth motion
-        get_batch().write_output(job.output, str(outputs['release.tar.gz']).removesuffix('.release.tar.gz'))
-
-        return self.make_outputs(multicohort, data=outputs, jobs=job)
